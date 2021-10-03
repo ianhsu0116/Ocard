8d4f3478d7f9abc9011c6d96649473f459d6c9f7
IanHsu <ianhsu0116@gmail.com>
2021-10-03 10:52:33 +0800

    主頁置中問題解決 herader還未解決

--- client/src/components/header-component.js
+++ client/src/components/header-component.js
@@ -6,25 +6,27 @@ import ocardLogo from "./images/Ocard.svg";
 
 const HeaderComponent = () => {
   return (
-    <header className="header">
-      <div className="header-left">
-        <div className="ham_menu">{NavigationIcons.HamMenuIcon()}</div>
-        <div className="logo">
-          <Link to="/">
-            <img src={ocardLogo} alt="Ocard Logo"></img>
+    <div className="header-con">
+      <header className="header">
+        <div className="header-left">
+          <div className="ham_menu">{NavigationIcons.HamMenuIcon()}</div>
+          <div className="logo">
+            <Link to="/">
+              <img src={ocardLogo} alt="Ocard Logo"></img>
+            </Link>
+          </div>
+          <div className="search">
+            <input placeholder="搜尋"></input>
+            <button>{NavigationIcons.SearchIcon()}</button>
+          </div>
+        </div>
+        <div className="header-right">
+          <Link to="/login">
+            <span>註冊 | 登入</span>
           </Link>
         </div>
-        <div className="search">
-          <input placeholder="搜尋"></input>
-          <button>{NavigationIcons.SearchIcon()}</button>
-        </div>
-      </div>
-      <div className="header-right">
-        <Link to="/login">
-          <span>註冊 | 登入</span>
-        </Link>
-      </div>
-    </header>
+      </header>
+    </div>
   );
 };
 
